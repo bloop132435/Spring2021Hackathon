8009bfea5ecfec0d1181fd96b41430f42c785e38
bloop132435 <geoffrey.t.qian@gmail.com>
2021-02-21 10:40:24 -0800

    fixed the algoTest file

--- algoTest.py
+++ algoTest.py
@@ -1,6 +1,6 @@
 import os
 from parse import parse
-
+from time import sleep
 testData = parse()
 algoIn = open("algoIn.txt","w")
 algoIn.write(str(len(testData)) + " " + "\n")
@@ -8,4 +8,5 @@ for entry in testData:
     for i in range(0,49):
         algoIn.write(str(entry[i]) + " ")
     algoIn.write("\n")
+algoIn.close()
 os.system("./a.out < algoIn.txt")
