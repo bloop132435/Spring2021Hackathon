4892c64f6c0975398fb9ec6fee0cecf53772d0a0
bloop132435 <geoffrey.t.qian@gmail.com>
2021-02-20 16:40:48 -0800

    added filling out form button

--- ux.py
+++ ux.py
@@ -1,5 +1,10 @@
 from tkinter import *
 import time
+import webbrowser
+def openform():
+	webbrowser.open(url = "https://docs.google.com/forms/d/e/1FAIpQLSd7PL7Kn-bcslvE4KVNeVY5gA47EowiQQfpkZnlZVyqXCg_VA/viewform?usp=sf_link")
 root = Tk()
 root.geometry('100x100+100+100') # size/position of root
+fillOut = Button(root,text = "fill out form",command = openform)
+fillOut.pack()
 root.mainloop()
