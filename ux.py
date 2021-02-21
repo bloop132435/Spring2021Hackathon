0a428c40c71680cd7ee283a867b7937213e40ba3
bloop132435 <geoffrey.t.qian@gmail.com>
2021-02-20 19:16:30 -0800

    change from 1hr to 30min

--- ux.py
+++ ux.py
@@ -12,10 +12,10 @@ root.title("Meeting Bot")
 root.geometry('10000x1000+100+100')  # size/position of root
 fillOut = Button(root, text="fill out form", command=openform,
                  font=("Fira Code", "8"), padx=0)
-fillOut.grid(row=0, column=0)
-for i in range(0, 24):
-    active.append(False)
-    tmpLbl = Label(root, text=indexToStr(i), bg="green" if active[i] else "white", padx=34)
+fillOut.place(x=00, y=100)
+for i in range(0, 48):
+    active.append(i%2==0)
+    tmpLbl = Label(root, text=indexToStr(i), bg="green" if active[i] else "white", padx=0)
     timeLine.append(tmpLbl)
     timeLine[i].grid(row=1, column=i)
 root.mainloop()
--- uxFuncs.py
+++ uxFuncs.py
@@ -1,11 +1,17 @@
 
+from math import floor
+
+
 def updateTimeLine():
-    for i in range(0,24):
+    for i in range(0,48):
         if active[i]==True:
             timeLine[i].configure(foreground="green")
 def indexToStr(num:int):
-    string = str(num)+":00"
-    return string;
+    realNum = num//2
+    string = str(realNum)
+    string+=":"
+    string+=("30" if num%2==1 else "00")
+    return string
 def openform():
     webbrowser.open(
         url="https://docs.google.com/forms/d/e/1FAIpQLSd7PL7Kn-bcslvE4KVNeVY5gA47EowiQQfpkZnlZVyqXCg_VA/viewform?usp=sf_link")
